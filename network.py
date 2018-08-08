41b7f9cbe1e25158fc85e81930969c33719febf6
BlueDespero <32748309+BlueDespero@users.noreply.github.com>
2018-08-08 18:00:58 +0200

    Update network.py

--- network.py
+++ network.py
@@ -1,12 +1,13 @@
 from keras import Sequential
 from keras.layers import Dense
 import random
+import numpy as np
 import string
 
 args = {'n_layers': 1, 
         'n_activation': 'sigmoid',
         'n_neurons': 16,
-        'hm_models': 100,
+        'hm_models': 1,
         'input_shape': 6,
         'output_shape': 3}
 
@@ -16,7 +17,7 @@ def generate_networks(args):
     for _ in range(args['hm_models']):
         model = Sequential()
         model.add(Dense(units = args['n_neurons'], input_shape = (1,args['input_shape']), activation = 'sigmoid'))
-        model.add(Dense(units = args['output_shape'], input_shape = (1,args['n_neurons']), activation = 'softmax'))
+        model.add(Dense(units = args['output_shape'], activation = 'softmax'))
         #print(model.summary())
         model_pocket.append(model)
 
@@ -38,4 +39,13 @@ def generate_networks(args):
 
 
 
-generate_networks(args)
+if __name__ == '__main__':
+    false_input = [0,0,1,0,0,1]
+    false_input = np.reshape(false_input, newshape=(1,1,6))
+    pack = generate_networks(args)
+    network = pack[0][1]
+    #print(network.summary())
+    prediction = network.predict(false_input)
+    print(prediction)
+    result = np.argmax(prediction,2)
+    print(result)
